06d478405ea39f4839f610b734e7853f8da1da62
spartha343 <spartha343@gmail.com>
2022-04-18 23:13:24 +0600

    final commit

--- src/Components/Pages/Bolog/Blog.js
+++ src/Components/Pages/Bolog/Blog.js
@@ -4,7 +4,7 @@ const Blog = () => {
     return (
         <div className='p-5 m-5'>
             <h1>What is the difference between authentication and authorization?</h1>
-            <h4 className='pb-4'>authentication is the process of verifying who someone is, and authorization is the process of verifying what
+            <h4 className='pb-4'>Authentication is the process of verifying who someone is, and authorization is the process of verifying what
                 specific applications, files, and data a
                 user has access to.</h4>
             <h1>why use firebase?</h1>
